29a85875c79f68d023dc83d134d9ba9e18318f75
Laura Nagy <ngylaura@gmail.com>
2018-10-12 12:57:16 +0100

    initial commit for testing

---
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,26 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  });
+  it('can perform addition', function(){
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 5);
+  });
+  it('can perform substraction', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 3);
+  })
+
+});
